ba138ff25ce27a49e673a998cb0fbd255f8109ca
Francesco Sardone <dev.airscript@gmail.com>
2021-10-30 16:38:41 -0700

    feature: added bats.sh script (#7)

---
+++ scripts/bats.sh
@@ -0,0 +1,21 @@
+# Denvair, your development environment made simple.
+# Copyright (C) 2021, Francesco Sardone <dev.airscript@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+echo "Installing bats..."
+git clone https://github.com/bats-core/bats-core.git
+cd bats-core
+./install.sh $HOME
+rm -rf ../bats-core
