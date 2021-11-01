f7f14b9fed2f7bdc8e5d2f615e9ce9ccf0f14172
Airscripts <dev.airscript@gmail.com>
2021-11-01 11:47:10 -0700

    test: tested mongo.sh script

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -14,13 +14,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#!/bin/sh
+
+# Declaring variables.
+SUPER="sudo"
+DRY_RUN=""
+
+# Checking if we're running script in test environment.
+if [ $1 = true ]; then
+  SUPER=""
+  DRY_RUN="--dry-run"
+fi
+
 echo "Installing MongoDB..."
-sudo apt -y install gnupg
+if ! $SUPER apt -y install gnupg $DRY_RUN ; then
+  exit 1
+fi
+
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl daemon-reload
-sudo systemctl start mongod
-sudo systemctl status mongod
-sudo systemctl enable mongod
+
+if ! $SUPER apt -y install mongodb-org $DRY_RUN ; then
+  exit 1
+fi
+
+if [ ! $1 ]; then
+  sudo systemctl daemon-reload
+  sudo systemctl start mongod
+  sudo systemctl status mongod
+  sudo systemctl enable mongod
+fi
